9ef02a108caf06cff829cba6ffa5473ac41e01c5
xd-zede32x <homekor24@gmail.com>
2024-07-22 15:24:08 +0300

    Прошел ещё один урок с Animator :heavy_check_mark:

---
+++ Developer's Shop/Assets/13Lesson/Scripts/UIWidgetButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ButtonScripts
+{
+    public class UIWidgetButton : MonoBehaviour
+    {
+        [SerializeField] private UIWidgetButtonAnimator _buttonAnimator;
+        [SerializeField] private float _lifeTime = 3f;
+
+        private void Awake()
+        => _buttonAnimator.OnAppearAnimationOverEvent += OnAppearAnimationOver;
+
+        private void OnDestroy()
+        => _buttonAnimator.OnAppearAnimationOverEvent -= OnAppearAnimationOver;
+
+        private void OnAppearAnimationOver()
+        => StartCoroutine(LifeTimeButton());
+
+        private IEnumerator LifeTimeButton()
+        {
+            yield return new WaitForSeconds(_lifeTime);
+
+            _buttonAnimator.PlayHide();
+        }
+    }
+}
---
+++ Developer's Shop/Assets/13Lesson/Scripts/UIWidgetButtonAnimator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace ButtonScripts
+{
+    public class UIWidgetButtonAnimator : MonoBehaviour
+    {
+        private const string Hide = nameof(Hide);
+
+        public event UnityAction OnAppearAnimationOverEvent;
+
+        [SerializeField] private Animator _animator;
+
+        private void OnValidate()
+        => _animator = GetComponent<Animator>();
+
+        private void Start()
+        => _animator ??= GetComponent<Animator>();
+
+        public void PlayHide()
+        => _animator.SetTrigger(Hide);
+
+        private void HandleAppearAnimationOver()
+        => OnAppearAnimationOverEvent?.Invoke();
+    }
+}
